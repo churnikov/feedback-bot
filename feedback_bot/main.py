2f459a462b554b9cfa4da0ae424183570d1081c5
Nikita Churikov <nikita@chur.ru>
2023-03-13 13:28:42 +0100

    update dependencies

--- feedback_bot/main.py
+++ feedback_bot/main.py
@@ -1,36 +1,33 @@
 # Start the bot
 # Create the Updater and pass it your bot's token.
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import CommandHandler, filters, MessageHandler, Application
 
 from feedback_bot.bot import error, forward_callback, help_callback, reply_callback, start_callback
-from feedback_bot.config import PROXY, TG_TOKEN
+from feedback_bot.config import TG_TOKEN
 
 
 def main():
-    updater = Updater(
+    app = Application.builder().token(
         token=TG_TOKEN,
-        use_context=True,
-        request_kwargs={"proxy_url": PROXY} if PROXY is not None else None,
     )
 
     # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    dp = app.build()
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start_callback))
     dp.add_handler(CommandHandler("help", help_callback))
 
     # Messages handler
-    dp.add_handler(MessageHandler(Filters.reply, reply_callback))
-    dp.add_handler(MessageHandler(Filters.all, forward_callback))
+    dp.add_handler(MessageHandler(filters.REPLY, reply_callback))
+    dp.add_handler(MessageHandler(filters.ALL, forward_callback))
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    dp.run_polling()
 
-    updater.idle()
 
 
 if __name__ == "__main__":
